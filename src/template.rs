33543837cc125dbd587743463f2ab6fa15594e6f
din0x <superrobertxd52@gmail.com>
2026-06-11 22:30:43 +0200

    Move layout() to template.rs

--- src/template.rs
+++ src/template.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use gen_html::{Render, html};
+use gen_html::{DOCTYPE, Render, html};
 
 pub fn link(r: impl Render, href: &str) -> impl Render {
     html! {
@@ -20,7 +20,7 @@ pub fn link(r: impl Render, href: &str) -> impl Render {
 
 pub fn frame(r: impl Render) -> impl Render {
     html! {
-        div ."p-2 mb-2 rounded-lg border-2 text-mist-400 border-mist-800 bg-mist-900 break-inside-avoid" {
+        div ."p-2 mb-2 rounded-lg border-2 border-mist-800 text-mist-400 bg-mist-900 break-inside-avoid" {
             (r)
         }
     }
@@ -85,4 +85,84 @@ badges! {
     UBX => "#ff4b4b",
     BLENDER => "#ffa754",
     UNREAL_ENGINE => "#888bed"
+}
+
+#[derive(Clone, Copy)]
+enum Target {
+    Here,
+    Blank,
+}
+
+impl Target {
+    fn value(self) -> Option<&'static str> {
+        match self {
+            Self::Here => None,
+            Self::Blank => Some("_blank"),
+        }
+    }
+}
+
+pub fn layout(title: &str, path: &str, content: impl Render) -> String {
+    let pages = [
+        ("projects", "/", Target::Here),
+        ("blog", "/blog", Target::Here),
+        ("experience", "/experience", Target::Here),
+        ("github", "https://github.com/din0x", Target::Blank),
+    ];
+
+    html! {
+        (DOCTYPE)
+        html ."scheme-only-dark" {
+            head {
+                meta charset: "UTF-8";
+                meta name: "viewport" content: "width=device-width, initial-scale=1.0";
+                title { (title) }
+                link rel: "stylesheet" href: "/assets/css.css" ;
+                script src: "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" {}
+            }
+            body ."bg-mist-950 text-mist-300" {
+                div ."px-6 w-full flex flex-col items-center bg-gray-900 border-b-2 border-gray-800" {
+                    div ."py-4 w-full max-w-240" {
+                        nav ."px-2 flex gap-8 text-mist-300 font-mono text-xl justify-end" {
+                            for (name, href, target) in pages {
+                                (nav_link(name, href, target, href == path))
+                            }
+                        }
+                    }
+                }
+                div ."min-h-dvh" {
+                    (content)
+                }
+                (footer())
+            }
+        }
+    }
+    .to_string()
+}
+
+fn nav_link(name: &str, href: &str, target: Target, is_active: bool) -> impl Render {
+    html! {
+        let text = (is_active)
+            .then_some("text-blue-400 font-medium")
+            .unwrap_or("hover:underline");
+
+        a .(format!("decoration-2 decoration-blue-400 {text}"))
+            href: (href)
+            target: (target.value())
+        {
+            (name)
+        }
+    }
+}
+
+fn footer() -> impl Render {
+    html! {
+        div ."px-6 w-full flex flex-col items-center bg-gray-900 border-t-2 border-gray-800" {
+            div ."py-4 w-full max-w-240 py-1 px-2 text-mist-300 font-mono text-xl" {
+                p ."text-center text-blue-400 font-medium decoration-2 decoration-blue-400 cursor-pointer hover:underline"  onclick: "window.scrollTo({ top: 0, behavior: 'smooth' });" {
+                    "Back to the top ↑"
+                }
+            }
+        }
+    }
 }
