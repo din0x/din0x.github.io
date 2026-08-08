a73b5525aee1eb4e0a02c3b5da0208aaed87c435
din0x <superrobertxd52@gmail.com>
2026-08-09 00:04:36 +0200

    feat: Add the logo

--- src/template.rs
+++ src/template.rs
@@ -131,6 +131,7 @@ pub fn layout(title: &str, path: &str, content: impl Render) -> String {
                 meta charset: "UTF-8";
                 meta name: "viewport" content: "width=device-width, initial-scale=1.0";
                 title { (title) }
+                link rel: "icon" r#type: "image/svg+xml" href: "/assets/r.svg";
                 link rel: "stylesheet" href: "/assets/css.css";
                 script src: "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" {}
             }
